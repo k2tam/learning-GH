70bb2a0abeb9955271f42fbee1505ced6427a645
k2tam <buingockhaitam01@gmail.com>
2022-07-06 13:15:32 +0700

    Initial Commit

---
+++ learning-GH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learning-GH
+//
+//  Created by k2 tam on 06/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ learning-GH/learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_GHApp.swift
+//  learning-GH
+//
+//  Created by k2 tam on 06/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
